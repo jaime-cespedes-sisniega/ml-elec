60d344b7570f2d32e51bcbb102f04e2a7610c753
Jaime Céspedes Sisniega <jaime.cespedes.sisniega@gmail.com>
2021-11-18 10:17:22 +0100

    Add basic api

---
+++ api/main.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from pathlib import Path
+from typing import Dict
+
+from fastapi import FastAPI
+from api.utils import load_model
+from api.data_models import Request, Response
+
+api = FastAPI()
+
+model = load_model(path=Path('api', 'models',
+                             'model_pipeline.joblib'))
+
+
+@api.post('/',
+          response_model=Response,
+          status_code=200)
+def predict(data: Request):
+    timestamp = datetime.today().isoformat()
+    pred = _make_prediction(data)
+    response = {'timestamp': timestamp,
+                'class_pred': pred}
+    return response
+
+
+def _make_prediction(data):
+    pred_encoded = model.predict([[*data.dict().values()]])
+    pred = model.inverse_transform_target(y_encoded=pred_encoded)[0]
+    return pred
+
+
+@api.get('/health')
+async def health() -> Dict[str, str]:
+    """Health check function
+
+    :return: Health check dict
+    :rtype: Dict[str: str]
+    """
+    return {'Status': 'Ok!'}
