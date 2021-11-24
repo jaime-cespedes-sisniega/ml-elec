8532250ebfb15cd58eeac244403bcc2f1ecbb4de
Jaime Céspedes Sisniega <jaime.cespedes.sisniega@gmail.com>
2021-11-24 13:53:28 +0100

    Update ml-pipeline version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ml-pipeline',
-    version='0.1.0',
+    version='0.1.1',
     packages=['ml_pipeline'],
     install_requires=['scikit-learn==1.0.1']
 )
