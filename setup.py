5c787355d65d81d75bd2935040923bbb471c3730
Jaime Céspedes Sisniega <jaime.cespedes.sisniega@gmail.com>
2021-11-25 13:24:41 +0100

    Separate setup requirements from normal requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup
 
+with open('requirements/setup_requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='ml-pipeline',
-    version='0.1.1',
+    version='0.1.2',
     packages=['ml_pipeline'],
-    install_requires=['scikit-learn==1.0.1']
+    install_requires=required
 )
